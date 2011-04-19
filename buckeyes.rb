4438d6ae846c6092167688a332891f1631f41b51
Josh Schairbaum <joshua.schairbaum@gmail.com>
2011-04-18 22:27:21 -0500

    ready to launch

--- buckeyes.rb
+++ buckeyes.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-require 'active_support'
+
+require 'active_support/all'
 
 set :last_defeat, "11/23/2003"
 
@@ -7,7 +8,8 @@ get '/' do
   @days_since = (Date.civil(2003,11,23) - Date.parse(Time.now.to_s)).to_i.abs
   @title = "How many days since Michigan has beaten Ohio State?"
 
-  @time = Time.now.strftime("%I:%M %p")
+  Time.zone = "Eastern Time (US & Canada)"
+  @time = Time.zone.now.strftime("%I:%M %p")
 
   erb :index
 end
@@ -27,10 +29,13 @@ __END__
     <div id="content">
       <%= yield %>
     </div>
+    <div id="footer">
+      an <a href="https://twitter.com/jschairb">@<span class="danzig">jschairb</span></a> joint
+    </div>
   </body>
 </html>
 
 @@index
-<p id="logo"><img src="ohio-state-200px.png" /></p>
 <p id="days_since"><span id="days_count"><%= @days_since %></span> days.</p>
-<p>It's <span id="time"><%= @time %> CDT</span> in Columbus and Michigan still sucks.</p>
+<p id="logo"><img src="pitiful-michigan-fans.jpg" /></p>
+<p id="phrase">It's <span id="time"><%= @time %></span> in Columbus and Michigan still sucks.</p>
