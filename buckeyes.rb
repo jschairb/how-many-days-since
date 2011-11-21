22378e83becb3012ddadb2d95b06dae942fa81d9
Josh Schairbaum <joshua.schairbaum@gmail.com>
2011-11-20 20:08:42 -0600

    adding urchin code

--- buckeyes.rb
+++ buckeyes.rb
@@ -32,6 +32,19 @@ __END__
     <div id="footer">
       an <a href="https://twitter.com/jschairb">@<span class="danzig">jschairb</span></a> joint
     </div>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-1518490-11']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
   </body>
 </html>
 
