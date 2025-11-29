d4958d0a133ee2901f38a7431f7ffd99f2989568
Josh Schairbaum <joshua.schairbaum@gmail.com>
2025-11-29 15:26:16 -0600

    add astro-og-canvas for dynamic OG images

---
+++ src/pages/og.png.ts
@@ -0,0 +1,39 @@
+import { OGImageRoute } from 'astro-og-canvas';
+
+const MS_PER_DAY = 24 * 60 * 60 * 1000;
+const referenceDate = new Date(import.meta.env.REFERENCE_DATE);
+const now = new Date();
+const daysSince = Math.max(
+  Math.floor((now.getTime() - referenceDate.getTime()) / MS_PER_DAY),
+  0
+);
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  param: 'route',
+  pages: {
+    home: {
+      title: `${daysSince} days`,
+      description: 'How many days since Michigan has beaten Ohio State?',
+    },
+    'mo-carmen': {
+      title: 'Mo Carmen',
+      description: 'Born the moment Shawn Springs fell down in \'96',
+    },
+  },
+  getImageOptions: (_path, page) => {
+    return {
+      title: page.title,
+      description: page.description,
+      bgGradient: [[190, 33, 55]],
+      font: {
+        title: {
+          size: 120,
+          weight: 'Bold',
+        },
+        description: {
+          size: 48,
+        },
+      },
+    };
+  },
+});
