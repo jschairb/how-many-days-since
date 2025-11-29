3c8f6e2703cd63941886ad8d2114b5d014e0dc39
Josh Schairbaum <joshua.schairbaum@gmail.com>
2025-11-29 08:37:23 -0600

    adding ga4 tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   vite: {
     define: {
       'import.meta.env.REFERENCE_DATE': JSON.stringify('2024-11-30T05:00:00.000Z'),
+      'import.meta.env.GA_MEASUREMENT_ID': JSON.stringify('G-SLJZ2YGLV1'), // Replace with your GA4 Measurement ID
     },
   },
 });
